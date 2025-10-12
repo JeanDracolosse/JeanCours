786740ddda7d2a6dc08300d4046c65d6756612aa
Vincent <vincent.achard3@gmail.com>
2025-10-12 19:01:50 +0200

    test

---
+++ app/routes/home.tsx
@@ -0,0 +1,25 @@
+
+import { useLoaderData } from "react-router";
+import DistanceChart from "~/components/distanceChart";
+import HrTImeInZoneChart from "~/components/hrTImeInZoneChart";
+import { getDistance, getHrTimeInZone, getIndex } from "~/utils/mongo";
+
+
+export async function loader() {
+  const hrTimeInZone = await getHrTimeInZone();
+  const distance = await getDistance()
+  const index = await getIndex()
+
+  return { hrTimeInZone, distance, index };
+}
+
+export default function Home() {
+  const { hrTimeInZone, distance, index } = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <HrTImeInZoneChart index={index} hrTimeInZone={hrTimeInZone} />
+      <DistanceChart index={index} distance={distance} />
+    </div>
+  );
+}
