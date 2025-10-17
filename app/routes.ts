dffc6dd4642d4d3ccddcbeeec89072c11538cb65
Vincent <vincent.achard3@gmail.com>
2025-10-17 06:56:55 +0200

    Motion framer

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,3 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"), route("charts", "routes/charts.tsx"), route("weekTypes", "routes/weekTypes.tsx")] satisfies RouteConfig;
+export default [index("routes/home.tsx"), route("test", "routes/weekTypesCards.tsx"), route("charts", "routes/charts.tsx"), route("weekTypes", "routes/weekTypes.tsx")] satisfies RouteConfig;
