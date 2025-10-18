de8e61679b85c31fb4403e86d76ec99beb2696ca
Vincent <vincent.achard3@gmail.com>
2025-10-18 10:29:12 +0200

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 120,
+  trailingComma: "es5",
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: "always",
+  jsxSingleQuote: false,
+  endOfLine: "lf",
+};
+
+export default config;
